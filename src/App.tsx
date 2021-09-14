9c2f6ae2fb3687175feb4b3f8c67f075e8378dce
Nam Pham <phamnam3@hotmail.com>
2021-09-13 23:58:26 -0400

    Done with explorer and adventure mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
 import snowman0 from './images/step_0.png'
+import snowman1 from './images/step_1.png'
+import snowman2 from './images/step_2.png'
+import snowman3 from './images/step_3.png'
+import snowman4 from './images/step_4.png'
+import snowman5 from './images/step_5.png'
+import snowman6 from './images/step_6.png'
+import snowman7 from './images/step_7.png'
 
 export function App() {
   const ALPHABET = [
@@ -34,8 +41,12 @@ export function App() {
   const [wordToGuess, setWordToGuess] = useState('')
   const [gameWord, setGameWord] = useState('')
   const [lettersGuessed, setLettersGuessed] = useState([''])
+  const [numberOfCorrectLettersGuessed, setNumberOfCorrectLettersGuessed] =
+    useState(0)
 
-  async function handleStartGameClick() {
+  async function handleNewGameClick() {
+    setLettersGuessed([])
+    setNumberOfCorrectLettersGuessed(0)
     const response = await fetch(
       'https://raw.githubusercontent.com/suncoast-devs/handbook/master/assignments/assets/words.json'
     )
@@ -50,6 +61,7 @@ export function App() {
   function handleClickOnLetterButton(letter: string) {
     setLettersGuessed([...lettersGuessed, letter])
     if (wordToGuess.includes(letter)) {
+      setNumberOfCorrectLettersGuessed(numberOfCorrectLettersGuessed + 1)
       let newGameWord = ''
       for (let i = 0; i < gameWord.length; i++) {
         if (wordToGuess[i] === letter) {
@@ -67,10 +79,31 @@ export function App() {
     }
   }
 
+  function getSnowmanPicture() {
+    switch (numberOfCorrectLettersGuessed) {
+      case 0:
+        return snowman0
+      case 1:
+        return snowman1
+      case 2:
+        return snowman2
+      case 3:
+        return snowman3
+      case 4:
+        return snowman4
+      case 5:
+        return snowman5
+      case 6:
+        return snowman6
+      case 7:
+        return snowman7
+    }
+  }
+
   console.log(`wordToGuess is ${wordToGuess}`)
   return (
     <div>
-      <img src={snowman0} width="500px" height="500px" />
+      <img src={getSnowmanPicture()} width="500px" height="500px" />
       <p>{gameWord}</p>
       {ALPHABET.map((letter) => {
         return (
@@ -83,7 +116,7 @@ export function App() {
           </button>
         )
       })}
-      <button onClick={handleStartGameClick}>Start Game</button>
+      <button onClick={handleNewGameClick}>New Game</button>
     </div>
   )
 }
